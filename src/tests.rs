313a6bab59779ffa265cba8d4c95baadcb6e6ea9
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-02-10 11:44:02 +0000

    Test reading a file

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,12 @@
+use std::{fs::File, io::Read};
+
 #[test]
-fn it_works() {
-    assert_eq!(1, 1);
+fn can_read_a_file() {
+    let mut buffer = [0; 100_000];
+
+    let mut file = File::open("sample.pdf").unwrap();
+
+    let bytes_read = file.read(&mut buffer).unwrap();
+
+    assert_ne!(bytes_read, 0);
 }
