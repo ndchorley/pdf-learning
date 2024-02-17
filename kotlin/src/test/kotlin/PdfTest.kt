9778bd4d7f3cc02b0c369ae0c869c81e593cc3cf
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-02-17 14:54:17 +0000

    Rename PdfTest to OpenPdfTest and remove an unused import

--- kotlin/src/test/kotlin/PdfTest.kt
+++ kotlin/src/test/kotlin/PdfTest.kt
@@ -1,10 +1,9 @@
 import com.lowagie.text.pdf.PdfName
 import com.lowagie.text.pdf.PdfReader
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
-class PdfTest {
+class OpenPdfTest {
     @Test
     fun `can find the page tree`() {
         val reader = PdfReader("sample.pdf")
